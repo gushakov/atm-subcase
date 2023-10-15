098e20a0b8264a3837bd64401d1ad2a78f36d9f3
gushakov <Georgy.Ushakov@unil.ch>
2023-10-15 16:18:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Subcases in CA
+
+Code illustrating the sharing functionality between use cases in Clean Architecture.
---
+++ src/test/java/com/github/atmsubcase/AtmSubcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.atmsubcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSubcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
